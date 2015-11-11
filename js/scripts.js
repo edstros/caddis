5a407682e9652fa080910015acbf48306dea5def
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-11-11 00:09:57 -0600

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 $(function () { // document ready
-/*================
-STICKY SIDEBAR
-  */
-
+  /*================
+  STICKY SIDEBAR
+   ================ */
   if (!!$('.sticky').offset()) { // make sure ".sticky" element exists
     var stickyTop = $('.sticky').offset().top; // returns number
     $(window).scroll(function () { // scroll event
@@ -18,60 +17,60 @@ STICKY SIDEBAR
     });
   }
 
-  /*=================
-  SIDEBAR MENU WITH JQUERY UI
-  */
+});
 
-   $( "#menu" ).menu();
+/*==========================
+SIDEBAR MENU WITH JQUERY UI
+============================ */
+$("#menu").menu();
 
+
+/*=============================
+ROLLOVER EFFECT ON ROUND IMAGES
+===============================*/
+// opacity of button set to 0%
+$('.roll').css('opacity', '0');
+// on mouse over
+$('.roll').hover(function () {
+    // set opacity to 70%
+    $(this).stop().animate({
+      opacity: .7
+    }, 'fast');
+  },
+  // on mouse out
+  function () {
+    // set opacity back to 0%
+    $(this).stop().animate({
+      opacity: 0
+    }, 'slow');
   });
 
-/*=====================
-ROLLOVER EFFECT ON ROUND IMAGES
-*/
-
-// opacity of button set to 0%
-  $('.roll').css('opacity', '0');
-
-  // on mouse over
-  $('.roll').hover(function () {
-
-      // set opacity to 70%
-      $(this).stop().animate({
-        opacity: .7
-      }, 'fast');
-    },
-    // on mouse out
-    function () {
-
-      // set opacity back to 0%
-      $(this).stop().animate({
-        opacity: 0
-      }, 'slow');
-    });
-/*======================
+/*===============================
 MOVE SEARCH BAR FROM TOP RIGHT
 TO UNDER SIDEBAR AND BACK AGAIN
-*/
-
+================================= */
 $(window).scroll(function () {
   var top = $(this).scrollTop();
   if (top > 300) {
     $('#searchboxwrapper').appendTo('#sidebar');
     $('.spacer').appendTo('searchboxwrapper');
     $('#logcart-icons').appendTo('#searchboxwrapper');
-    $('#logcart-icons').css('margin', '10px').css('padding-left', '45px');
+    $('#logcart-icons').css({
+      margin: '10px',
+      'padding-left': '45px'
+    });
     $('.underside').fadeOut();
     $('.undersearch').fadeIn();
   } else {
     $('#logcart-icons').appendTo('#top-right');
-    $('#logcart-icons').css('margin-top', '0').css('margin-right', '10px').css('padding-left', '0');
+    $('#logcart-icons').css({
+      'margin-top': 0,
+      'margin-right': '10px',
+      'padding-left': 0
+    });
     $('#searchboxwrapper')
       .appendTo('#top-right');
-
     $('.underside').fadeIn();
     $('.undersearch').fadeOut();
   }
-
-
 });
