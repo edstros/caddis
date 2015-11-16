e0c4c7b7e91c9f40788f870020414a120abfeed7
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-11-16 15:36:16 -0600

    sub jpgs for pngs, change index to reflect that, tweaked jquery ui to fix sidebar pixel jumping, optimized js,

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,88 @@
-$(function () { // document ready
-  /*================
-  STICKY SIDEBAR
-   ================ */
-  if (!!$('.sticky').offset()) { // make sure ".sticky" element exists
-    var stickyTop = $('.sticky').offset().top; // returns number
-    $(window).scroll(function () { // scroll event
-      var windowTop = $(window).scrollTop(); // returns number
-      if (stickyTop < windowTop) {
-        $('.sticky').css({
+  var $sticky = $('.sticky');
+  var $win = $(window);
+  var $logcart = $('#logcart-icons');
+  var $searchwrap = $('#searchboxwrapper');
+  var $underside = $('.underside');
+  var $undersearch = $('.undersearch');
+  var $roll = $('.roll');
+
+  $(function () { // document ready
+    /*================
+    STICKY SIDEBAR
+     ================ */
+    var stickyTop = $sticky.offset().top; // returns number
+    $win.scroll(function () { // scroll event
+      var windowTop = $win.scrollTop(); // returns number
+      if (stickyTop - 50 < windowTop) {
+        $sticky.css({
           position: 'fixed',
           top: 0
         });
       } else {
-        $('.sticky').css('position', 'static');
+        $sticky.css('position', 'static');
+      }
+      /*===============================
+      MOVE SEARCH BAR FROM TOP RIGHT
+      TO UNDER SIDEBAR AND BACK AGAIN
+      ================================= */
+      if (windowTop > 300) {
+        $searchwrap
+          .appendTo('#sidebar')
+          .fadeIn()
+          .css({
+            marginTop: '10px'
+          });
+        $logcart
+          .appendTo($searchwrap)
+          .css({
+            margin: '10px',
+            paddingLeft: '45px'
+          });
+        $underside.slideUp();
+        $undersearch.slideUp();
+      } else {
+        $logcart
+          .appendTo('#top-right')
+          .fadeIn()
+          .css({
+            marginTop: 0,
+            marginRight: '10px',
+            paddingLeft: 0
+          });
+        $searchwrap
+          .appendTo('#top-right')
+          .css({
+            margin: 0
+          });
+        $underside.fadeIn();
+        $undersearch.fadeOut();
       }
     });
-  }
 
-});
-
-/*==========================
-SIDEBAR MENU WITH JQUERY UI
-============================ */
-$("#menu").menu();
-
-
-/*=============================
-ROLLOVER EFFECT ON ROUND IMAGES
-===============================*/
-// opacity of button set to 0%
-$('.roll').css('opacity', '0');
-// on mouse over
-$('.roll').hover(function () {
-    // set opacity to 70%
-    $(this).stop().animate({
-      opacity: .7
-    }, 'fast');
-  },
-  // on mouse out
-  function () {
-    // set opacity back to 0%
-    $(this).stop().animate({
-      opacity: 0
-    }, 'slow');
+    /*==========================
+    SIDEBAR MENU WITH JQUERY UI
+    ============================ */
+    $("#menu").menu();
+    $('ui-menu-item').css({
+      borderTop: '1px solid #ccc'
+    });
+    /*=============================
+    ROLLOVER EFFECT ON ROUND IMAGES
+    ===============================*/
+    // opacity of button set to 0%
+    $roll.css('opacity', '0');
+    // on mouse over
+    $roll.hover(function () {
+        // set opacity to 70%
+        $(this).stop().animate({
+          opacity: 0.7
+        }, 'fast');
+      },
+      // on mouse out
+      function () {
+        // set opacity back to 0%
+        $(this).stop().animate({
+          opacity: 0
+        }, 'slow');
+      });
   });
-
-/*===============================
-MOVE SEARCH BAR FROM TOP RIGHT
-TO UNDER SIDEBAR AND BACK AGAIN
-================================= */
-$(window).scroll(function () {
-  var top = $(this).scrollTop();
-  if (top > 300) {
-    $('#searchboxwrapper').appendTo('#sidebar');
-    $('.spacer').appendTo('searchboxwrapper');
-    $('#logcart-icons').appendTo('#searchboxwrapper');
-    $('#logcart-icons').css({
-      margin: '10px',
-      'padding-left': '45px'
-    });
-    $('.underside').fadeOut();
-    $('.undersearch').fadeIn();
-  } else {
-    $('#logcart-icons').appendTo('#top-right');
-    $('#logcart-icons').css({
-      'margin-top': 0,
-      'margin-right': '10px',
-      'padding-left': 0
-    });
-    $('#searchboxwrapper')
-      .appendTo('#top-right');
-    $('.underside').fadeIn();
-    $('.undersearch').fadeOut();
-  }
-});
